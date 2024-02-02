0b084d3d6cd58dfb2049f49638032fabadf51c13
fredala <158566238+fredala@users.noreply.github.com>
2024-02-02 18:00:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -57,16 +57,7 @@ def Readfiche(post_id):
     return render_template('read_data.html', data=data)
 
 
-@app.route('/search/<str:nom>')
-def Search(nom):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', (nom,))
-    data = cursor.fetchall()
-    conn.close()
-    
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
+
                                                                                                                                  
 if __name__ == "__main__":
   app.run(debug=True)
